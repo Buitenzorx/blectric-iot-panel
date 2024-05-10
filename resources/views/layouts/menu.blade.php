ccdc572e10f2f870e598dd0cb87b8576005ae943
Bayu Sutra <202111224bayus@gmail.com>
2024-05-10 14:36:55 +0700

    Migration LED

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -32,7 +32,7 @@
             <li class="nav-item">
                 <a class="nav-link" href="{{ route('temperatures') }}">
                     <i class="fas fa-temperature-high"></i>
-                    <span>Monitoring Temperature</span></a>
+                    <span>Monitoring Temperatures</span></a>
             </li>
 
             <!-- Nav Item - Device -->
